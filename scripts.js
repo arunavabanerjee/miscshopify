099b3484be2fda37463e982eca3c3d6c5e12d458
arunavabanerjee <mrarunbanerjee09@gmail.com>
2017-07-15 14:24:22 +0000

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,29 @@
- 
+
+      <header class="header-note mt-header">
+        <div class="container">
+        <strong>Free Shipping • Free Returns • No Sales Tax</strong>
+        <div class="dis-count">
+          <form>
+          <ul>
+            <li><input type="text" name="enterDiscount" id="enterDiscount" placeholder="Discount"/></li>
+            <li><input type="hidden" name="discountVal" id="discountVal" value=""/></li>
+            <li><input type="submit" value="Apply" class="discount-btn"/></li>
+          </ul>
+          </form>
+        </div>
+        </div>
+      </header>
+
+    <script type="text/javascript">
+      jQuery(".discount-btn").click(function(){ 
+        var discountCode = jQuery('#enterDiscount').val(); alert(discountCode); 
+        sessionStorage.setItem("discountCode", discountCode);
+      });
+    </script>
+    <script>
+     jQuery('#discountVal').val(sessionStorage.getItem("discountCode"));
+    </script>  
+
  {% if template contains 'product' %}
     <script>
      jQuery(window).load(function(){ 
