bf9b49e4129d6ee2c81065418aebcf2c9dac4353
arunavabanerjee <mrarunbanerjee09@gmail.com>
2017-07-16 15:26:39 +0000

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -46,6 +46,41 @@
     </script>
  {% endif %}
  
+ ----------------
+   <script type="text/javascript">
+      jQuery(".discount-btn").click(function(){ 
+        var discountCode = jQuery('#enterDiscount').val(); alert(discountCode); 
+        //sessionStorage.setItem("discountCode", discountCode);
+        localStorage.setItem("discountCode", discountCode);
+      });
+    </script>
+    <script>
+     jQuery('#discountVal').val(localStorage.getItem("discountCode"));
+    </script>  
+    
+    {% if template contains 'product' %}
+    <script>
+     jQuery(window).load(function(){ 
+      var disCountValue = jQuery('#discountVal').val(); 
+      if( disCountValue != '' ){  
+       jQuery('#product-info .discount-codes div').each(function(){ 
+         if( jQuery(this).attr('coupon-title') == disCountValue ){
+           jQuery(this).css('display', 'block');
+           var pDiscount = jQuery(this).attr('data-discount'); 
+           var pDiscountInt = parseFloat(pDiscount);
+           var currPrice = jQuery('#product-info .product-price .detail-price .price .money').text().replace('$','');
+           var currPriceFlt = parseFloat(currPrice); 
+           var pDiscountPrice = currPriceFlt - ((pDiscountInt /100.00) * currPriceFlt); 
+           var pDiscountPriceFlt = '$' + parseFloat(pDiscountPrice).toFixed(2);
+           jQuery('#product-info .product-price .discount-price').text(pDiscountPriceFlt);
+         } 
+       });              
+      } 
+     });   
+    </script>
+    {% endif %}	 
+	 
+ 
  %---------------------------------------------------------------------------------------------
        
  <script type="text/javascript">
