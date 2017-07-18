b5f5afd3e8315c1c20aafbcb26999e22af60c851
arunavabanerjee <mrarunbanerjee09@gmail.com>
2017-07-18 11:16:28 +0000

    Rename emailtemplates.dd.php to emailtemplates.ddn.php

---
+++ emailtemplates.ddn.php
@@ -0,0 +1,58 @@
+<table cellspacing="0" cellpadding="0" border="0" width="98%" style="font:11px/1.35em Verdana, Arial, Helvetica, sans-serif;">
+  <tr>
+     <td align="center" valign="top">
+     <table cellspacing="0" cellpadding="0" border="0" width="550" height="594" style="background: #fff;">
+       <tr>
+        <td background="https://doordropdeliveries.co.uk/wp-content/themes/quickfood-child/images/main-bg.jpg">                            
+        <table width="90%" height="auto" align="center" cellpadding="0" cell-sapcing="0" style="margin-top: 90px;">
+          <tr>
+             <td>
+               <p style="font-size: 20px; line-height: 18px; color:#252525; text-align: center;">Sender Application Info<br></p>
+               <h1 style="color: #c18205;font-size: 26px; text-align: center;">Name: [firstname] [secondname] </h1>
+               <p style="font-size: 20px; line-height: 18px; color:#2e2b2b; text-align: center;">Email: [your-email] <br></p>
+               <p style="font-size: 20px; line-height: 18px; color:#2e2b2b; text-align: center;">Mobile No: [phone]<br></p>     
+	       <p style="font-size: 12px; line-height: 18px; color:#2e2b2b; text-align: center;">Have Bike/Scooter/Car: [bikescooter]<br></p>   
+                <p style="font-size: 12px; line-height: 18px; color:#2e2b2b; text-align: center;">Is Student: [student]<br></p>    
+                <p style="font-size: 12px; line-height: 18px; color:#2e2b2b; text-align: center;">Has License:[license]<br/></p>  
+		<p style="font-size: 12px; line-height: 18px; color:#2e2b2b; text-align: center;">Has Android Mobile: [mobile]<br></p>               
+             </td>
+         </tr>
+   <tr><td>&nbsp;</td></tr>                                                                                             
+   <tr><td style="line-height: 38px !important; padding: 26px 0 0 0;"></td></tr>
+</table>                            
+</td>                        
+</tr>                   
+</table>
+</td>
+</tr>
+</table>
+=============================================== 
+
+<table cellspacing="0" cellpadding="0" border="0" width="98%" style="font:11px/1.35em Verdana, Arial, Helvetica, sans-serif;">
+  <tr>
+     <td align="center" valign="top">
+     <table cellspacing="0" cellpadding="0" border="0" width="550" height="594" style="background: #fff;">
+       <tr>
+        <td background="https://doordropdeliveries.co.uk/wp-content/themes/quickfood-child/images/main-bg.jpg">                            
+        <table width="90%" height="auto" align="center" cellpadding="0" cell-sapcing="0" style="margin-top: 90px;">
+          <tr>
+             <td>
+               <p style="font-size: 20px; line-height: 18px; color:#252525; text-align: center;">Sender Application Info<br></p>
+               <h1 style="color: #c18205;font-size: 26px; text-align: center;">Name: [firstname] [lastname] </h1>
+               <p style="font-size: 20px; line-height: 18px; color:#2e2b2b; text-align: center;">Email: [your-email] <br></p>
+               <p style="font-size: 20px; line-height: 18px; color:#2e2b2b; text-align: center;">Mobile/Phone: [phone]<br></p>     
+	       <p style="font-size: 12px; line-height: 18px; color:#2e2b2b; text-align: center;">Restaurant Name: [resturentname]<br></p>   
+                <p style="font-size: 12px; line-height: 18px; color:#2e2b2b; text-align: center;">Website: [website]<br></p>    
+                <p style="font-size: 12px; line-height: 18px; color:#2e2b2b; text-align: center;">City/PostCode:[city] / [postcode]<br/></p>  
+		<p style="font-size: 12px; line-height: 18px; color:#2e2b2b; text-align: center;">Password: [createpassword]<br></p>               
+             </td>
+         </tr>
+       <tr><td>&nbsp;</td></tr>                                                                                             
+       <tr><td style="line-height: 38px !important; padding: 26px 0 0 0;"></td></tr>
+      </table>                            
+      </td>                        
+     </tr>                   
+   </table>
+  </td>
+  </tr>
+</table>
